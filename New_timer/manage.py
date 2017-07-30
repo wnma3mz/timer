95eaa0f1919174067e0c30a5614ef04c19d30b2d
wnma3mz <wnma3mz@163.com>
2017-07-30 09:42:33 +0800

    3.0

--- New_timer/manage.py
+++ New_timer/manage.py
@@ -19,5 +19,5 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    manager.run()
 
